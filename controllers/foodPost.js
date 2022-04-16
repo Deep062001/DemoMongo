903d5dc47c08e0d5324a7fe9f51df308024b5a7c
Shrishti <shrishtigoyal2608@gmail.com>
2022-04-16 15:19:17 +0530

    like dislike and delete added

--- controllers/foodPost.js
+++ controllers/foodPost.js
@@ -1,4 +1,5 @@
 import FoodMessage from "../models/foodMessage.js";
+import mongoose from "mongoose";
 export const getFoodPost =async(req,res) =>{
    try {
        const foodMessages = await FoodMessage.find();
@@ -18,4 +19,25 @@ export const createFoodPost = async(req,res) =>{
  } catch (error) {
     res.status(409).json({message: error.message});
  }
+}
+export const deleteFoodPost = async(req,res) =>{
+    const {id} = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    await FoodMessage.findByIdAndRemove(id);
+    res.json({ message: "Post deleted successfully." });
+    
+}
+export const likeFoodPost = async(req,res) =>{
+    const {id} = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    const foodPost = await FoodMessage.findById(id);
+    const updateFoodPost =  await FoodMessage.findByIdAndUpdate(id, { likeCount: foodPost.likeCount + 1 }, { new: true });
+    res.json(updateFoodPost);
+}
+export const dislikeFoodPost = async(req,res) =>{
+    const {id} = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    const foodPost = await FoodMessage.findById(id);
+    const updateFoodPost =  await FoodMessage.findByIdAndUpdate(id, { dislikeCount: foodPost.dislikeCount + 1 }, { new: true });
+    res.json(updateFoodPost);
 }
--- routes/food.js
+++ routes/food.js
@@ -1,7 +1,10 @@
 import express from "express";
-import { getFoodPost , createFoodPost} from "../controllers/foodPost.js";
+import { getFoodPost , createFoodPost, likeFoodPost, dislikeFoodPost , deleteFoodPost} from "../controllers/foodPost.js";
 const router = express.Router();
 
 router.get('/' , getFoodPost );
 router.post('/' , createFoodPost );
+router.patch('/:id/likePost' , likeFoodPost);
+router.patch('/:id/dislikePost' , dislikeFoodPost);
+router.delete('/:id' , deleteFoodPost);
 export default router;
