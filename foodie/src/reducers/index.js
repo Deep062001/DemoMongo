9e3b8e4ae9caf7479e7ad0919a2df3aa16904c57
Deepanshi Singh <t-deesingh@microsoft.com>
2022-08-27 01:49:13 +0530

    working

--- foodie/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-
-import foodPosts from './foodPosts';
-import shopPosts from './shopPosts';
-
-export const reducers = combineReducers({ foodPosts , shopPosts});
